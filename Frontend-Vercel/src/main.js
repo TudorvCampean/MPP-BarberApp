0870f798982de490863883b0859602381ccee562
Campean Tudor <tudorv.camepean@gmail.com>
2026-05-04 20:46:44 +0300

    link

--- Frontend-Vercel/src/main.js
+++ Frontend-Vercel/src/main.js
@@ -5,6 +5,6 @@ import './style.css';
 // Poți seta URL-ul de bază pentru axios aici, ca să nu îl schimbi în fiecare componentă.
 import axios from 'axios';
 // IMPORTANT: Înlocuiește acest link cu cel generat de `herd share` când ești gata de prezentare!
-axios.defaults.baseURL = 'https://mw324477w0.sharedwithexpose.com';
+axios.defaults.baseURL = 'https://pvl6j7b4en.sharedwithexpose.com';
 
 createApp(MainApp).mount('#app');
