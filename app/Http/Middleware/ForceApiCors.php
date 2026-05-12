ab7af62dcfa975611ed4ef20b8e43c42804f3523
Campean Tudor <tudorv.camepean@gmail.com>
2026-05-12 17:26:48 +0300

    local

--- app/Http/Middleware/ForceApiCors.php
+++ app/Http/Middleware/ForceApiCors.php
@@ -10,28 +10,6 @@ class ForceApiCors
 {
     public function handle(Request $request, Closure $next): Response
     {
-        // Păcălim browserul spunându-i că permitem exact adresa de pe care vine el, nu "*"
-        $origin = $request->header('Origin') ?: '*';
-
-        // Am adăugat 'ngrok-skip-browser-warning' în lista de headere permise!
-        $allowedHeaders = 'Content-Type, Authorization, Accept, X-Requested-With, ngrok-skip-browser-warning';
-
-        if ($request->isMethod('OPTIONS')) {
-            return response('', 200)
-                ->header('Access-Control-Allow-Origin', $origin)
-                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-                ->header('Access-Control-Allow-Headers', $allowedHeaders)
-                ->header('Access-Control-Allow-Credentials', 'true');
-        }
-
-        /** @var Response $response */
-        $response = $next($request);
-
-        $response->headers->set('Access-Control-Allow-Origin', $origin);
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', $allowedHeaders);
-        $response->headers->set('Access-Control-Allow-Credentials', 'true');
-
-        return $response;
+        return $next($request);
     }
 }
