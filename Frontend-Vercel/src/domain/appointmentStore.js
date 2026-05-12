d5e74bf1a39aab862eb944fa4025dc6158c20b9b
Campean Tudor <tudorv.camepean@gmail.com>
2026-05-12 17:06:05 +0300

    local

--- Frontend-Vercel/src/domain/appointmentStore.js
+++ Frontend-Vercel/src/domain/appointmentStore.js
@@ -73,7 +73,6 @@ const request = async (path = '', options = {}) => {
         headers: {
             Accept: 'application/json',
             'Content-Type': 'application/json',
-            '
             ...(options.headers || {}),
         },
         body: options.body,
