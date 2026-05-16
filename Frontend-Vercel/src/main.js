8ab1fe6dc4fa8d57da3d60f49549c1f5bac88329
Campean Tudor <tudorcampean795@gmail.com>
2026-05-16 21:02:01 +0300

    vm

--- Frontend-Vercel/src/main.js
+++ Frontend-Vercel/src/main.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 // ---------------------------------------------------------------------------
 // Am pus adresa IP direct aici pentru a ignora orice cache de .env
 // ---------------------------------------------------------------------------
-const __API_BASE__ = 'https://MPP-BarberApp.test/api';
+const __API_BASE__ = 'https://MPP-BarberApp.test';
 
 axios.defaults.baseURL = __API_BASE__;
 
